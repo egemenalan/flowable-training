878149e81697850e6577ab846fea0f71b7f5cb84
Egemen_Alan <egemen.alan@robusta.com.tr>
2019-01-30 09:24:29 +0300

    job

---
+++ modules/flowable-ui-admin/flowable-ui-admin-app/src/main/java/org/flowable/ui/admin/application/ContexListsener.java
@@ -0,0 +1,78 @@
+package org.flowable.ui.admin.application;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+/**
+ * @author Egemen Alan
+ */
+
+@WebListener
+public class ContexListsener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    	
+    	System.out.println(System.getProperty("user.dir"));
+    	String tomcatHomeDirectory = System.getProperty("catalina.base");
+    	
+    	String customerLogoFilePath = tomcatHomeDirectory + java.io.File.separator 
+    			+ "customer_configuration" + java.io.File.separator + "customer_logo.png";
+//    	String customerLogoFilePath = System.getProperty("user.dir")+ java.io.File.separator 
+//    			+ "customer_configuration" + java.io.File.separator + "customer_logo.png";
+    	
+    	String outputFilePath = tomcatHomeDirectory + java.io.File.separator 
+    			// FIXME prje ismi ve klasorler hard coded yazildi ileride degistirilirse sorun olur bunlari toplu bir konfigden almak gerek
+    			+ "wtpwebapps"  + java.io.File.separator + "flowable-ui-admin-app" + java.io.File.separator + "WEB-INF"  + java.io.File.separator + "classes" + java.io.File.separator + "static"
+    			//+ servletContextEvent.getServletContext().getContextPath()
+    			+ java.io.File.separator + "images" + java.io.File.separator +  "flowable-logo.png";
+    	
+//    	String outputFilePath = "wtpwebapps"  + java.io.File.separator + "flowable-ui-idm-app" + java.io.File.separator + "WEB-INF"  + java.io.File.separator + "classes" + java.io.File.separator + "static"
+//    			+ servletContextEvent.getServletContext().getContextPath()
+//    			+ java.io.File.separator + "images" + java.io.File.separator +  "flowable-logo.png";
+    			//C:\Flowable-Engine-WS\flowable-engine\modules\flowable-ui-idm\flowable-ui-idm-app\src\main
+    			
+    			//\wtpwebapps\flowable-ui-idm-app\WEB-INF\classes\static\images
+    			
+    			
+    			/*"flowable-ui-idm-app" + java.io.File.separator 
+    			+ "src" + java.io.File.separator 
+    			+ "main" + java.io.File.separator 
+    			+ "resources" + java.io.File.separator 
+    			+ "static" + java.io.File.separator 
+    			+ "images" + java.io.File.separator +  "flowable-logo@2x.png";
+    			*/
+    	//read
+    	//C:\Flowable-Engine-WS\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\customer_configuration\customer_logo.png
+    	//C:\Flowable-Engine-WS\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\webapps\flowable-ui-admin-app\images\flowable-logo.png
+    	
+    	//write
+    	
+    	BufferedImage img = null;
+    	try {
+    	    img = ImageIO.read(new File(customerLogoFilePath));
+    	    
+    	    ImageIO.write(img, "png", new File(outputFilePath));
+    	    
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	System.out.println(tomcatHomeDirectory);
+    	System.out.println(customerLogoFilePath);
+    	System.out.println(outputFilePath);
+    	
+    	System.out.println(servletContextEvent.getServletContext().getServerInfo());
+    	
+    	System.out.println(servletContextEvent.getServletContext().getContextPath());
+    	
+    	
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+    }
+	
+}
--- modules/flowable-ui-modeler/flowable-ui-modeler-rest/src/main/java/org/flowable/ui/modeler/rest/app/StencilSetResource.java
+++ modules/flowable-ui-modeler/flowable-ui-modeler-rest/src/main/java/org/flowable/ui/modeler/rest/app/StencilSetResource.java
@@ -12,8 +12,8 @@
  */
 package org.flowable.ui.modeler.rest.app;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.flowable.ui.common.service.exception.InternalServerErrorException;
 import org.slf4j.Logger;
@@ -23,34 +23,194 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
+
 @RestController
 @RequestMapping("/app")
 public class StencilSetResource {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StencilSetResource.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(StencilSetResource.class);
 
-    @Autowired
-    protected ObjectMapper objectMapper;
+	@Autowired
+	protected ObjectMapper objectMapper;
 
-    @RequestMapping(value = "/rest/stencil-sets/editor", method = RequestMethod.GET, produces = "application/json")
-    public JsonNode getStencilSetForEditor() {
-        try {
-            JsonNode stencilNode = objectMapper.readTree(this.getClass().getClassLoader().getResourceAsStream("stencilset_bpmn.json"));
-            return stencilNode;
-        } catch (Exception e) {
-            LOGGER.error("Error reading bpmn stencil set json", e);
-            throw new InternalServerErrorException("Error reading bpmn stencil set json");
+	@RequestMapping(value = "/rest/stencil-sets/editor", method = RequestMethod.GET, produces = "application/json")
+	public JsonNode getStencilSetForEditor() {
+		try {
+			JsonNode stencilNode = objectMapper
+					.readTree(this.getClass().getClassLoader().getResourceAsStream("stencilset_bpmn.json"));
+			JsonNode robusta = objectMapper
+					.readTree(this.getClass().getClassLoader().getResourceAsStream("custom_stencilset_webapp.json"));
+			return merge(stencilNode, robusta);
+		} catch (Exception e) {
+			LOGGER.error("Error reading bpmn stencil set json", e);
+			throw new InternalServerErrorException("Error reading bpmn stencil set json");
+		}
+	}
+
+	@RequestMapping(value = "/rest/stencil-sets/cmmneditor", method = RequestMethod.GET, produces = "application/json")
+	public JsonNode getCmmnStencilSetForEditor() {
+		try {
+			JsonNode stencilNode = objectMapper
+					.readTree(this.getClass().getClassLoader().getResourceAsStream("stencilset_cmmn.json"));
+			JsonNode robusta = objectMapper
+					.readTree(this.getClass().getClassLoader().getResourceAsStream("custom_stencilset_webapp.json"));
+			return merge(stencilNode, robusta);
+		} catch (Exception e) {
+			LOGGER.error("Error reading bpmn stencil set json", e);
+			throw new InternalServerErrorException("Error reading bpmn stencil set json");
+		}
+	}
+	
+//	public static JsonNode merge(JsonNode mainNode, JsonNode updateNode) {
+//		
+//		if (updateNode == null)
+//			return mainNode;
+//
+//		Iterator<String> fieldNames = updateNode.fieldNames();
+//		while (fieldNames.hasNext()) {
+//
+//			String fieldName = fieldNames.next();
+//			JsonNode jsonNode = mainNode.get(fieldName);
+//			// if field exists and is an embedded object
+//			if (jsonNode != null && !jsonNode.isObject()) {
+//				merge(jsonNode, updateNode.get(fieldName));
+//			} else {
+//				if (mainNode instanceof ObjectNode) {
+//					// Overwrite field
+//					JsonNode value = updateNode.get(fieldName);
+//					((ObjectNode) mainNode).put(fieldName, value);
+//				}
+//			}
+//
+//		}
+//
+//		return mainNode;
+//	}
+//}
+	
+	/**
+	 * Merge two JSON tree into one i.e mergedInTo.
+	 *
+	 * @param toBeMerged
+	 * @param mergedInTo
+	 * @return 
+	 */
+//	public static JsonNode merge(JsonNode toBeMerged, JsonNode mergedInTo) {
+//	    Iterator<Map.Entry<String, JsonNode>> incomingFieldsIterator = toBeMerged.fields();
+//	    Iterator<Map.Entry<String, JsonNode>> mergedIterator = mergedInTo.fields();
+//
+//	    while (incomingFieldsIterator.hasNext()) {
+//	        Map.Entry<String, JsonNode> incomingEntry = incomingFieldsIterator.next();
+//
+//	        JsonNode subNode = incomingEntry.getValue();
+//
+//	        if (subNode.getNodeType().equals(JsonNodeType.OBJECT)) {
+//	            boolean isNewBlock = true;
+//	            mergedIterator = mergedInTo.fields();
+//	            while (mergedIterator.hasNext()) {
+//	                Map.Entry<String, JsonNode> entry = mergedIterator.next();
+//	                if (entry.getKey().equals(incomingEntry.getKey())) {
+//	                    merge(incomingEntry.getValue(), entry.getValue());
+//	                    isNewBlock = false;
+//	                }
+//	            }
+//	            if (isNewBlock) {
+//	                ((ObjectNode) mergedInTo).replace(incomingEntry.getKey(), incomingEntry.getValue());
+//	            }
+//	        } else if (subNode.getNodeType().equals(JsonNodeType.ARRAY)) {
+//	            boolean newEntry = true;
+//	            mergedIterator = mergedInTo.fields();
+//	            while (mergedIterator.hasNext()) {
+//	                Map.Entry<String, JsonNode> entry = mergedIterator.next();
+//	                if (entry.getKey().equals(incomingEntry.getKey())) {
+//	                    updateArray(incomingEntry.getValue(), entry);
+//	                    newEntry = false;
+//	                }
+//	            }
+//	            if (newEntry) {
+//	                ((ObjectNode) mergedInTo).replace(incomingEntry.getKey(), incomingEntry.getValue());
+//	            }
+//	        }
+//	        
+//	        ValueNode valueNode = null;
+//	        JsonNode incomingValueNode = incomingEntry.getValue();
+//	        switch (subNode.getNodeType()) {
+//	            case STRING:
+//	                valueNode = new TextNode(incomingValueNode.textValue());
+//	                break;
+//	            case NUMBER:
+//	                valueNode = new IntNode(incomingValueNode.intValue());
+//	                break;
+//	            case BOOLEAN:
+//	                valueNode = BooleanNode.valueOf(incomingValueNode.booleanValue());
+//	        }
+//	        if (valueNode != null) {
+//	            updateObject(mergedInTo, valueNode, incomingEntry);
+//	        }
+//	    }
+//	    return toBeMerged;
+//	}
+//
+//	private static void updateArray(JsonNode valueToBePlaced, Map.Entry<String, JsonNode> toBeMerged) {
+//	    toBeMerged.setValue(valueToBePlaced);
+//	}
+//
+//	private static void updateObject(JsonNode mergeInTo, ValueNode valueToBePlaced,
+//	                                 Map.Entry<String, JsonNode> toBeMerged) {
+//	    boolean newEntry = true;
+//	    Iterator<Map.Entry<String, JsonNode>> mergedIterator = mergeInTo.fields();
+//	    while (mergedIterator.hasNext()) {
+//	        Map.Entry<String, JsonNode> entry = mergedIterator.next();
+//	        if (entry.getKey().equals(toBeMerged.getKey())) {
+//	            newEntry = false;
+//	            entry.setValue(valueToBePlaced);
+//	        }
+//	    }
+//	    if (newEntry) {
+//	        ((ObjectNode) mergeInTo).replace(toBeMerged.getKey(), toBeMerged.getValue());
+//	    }
+//	}
+	public static JsonNode merge(JsonNode mainNode, JsonNode updateNode) {
+	Iterator<String> fieldNames = updateNode.fieldNames();
+
+    while (fieldNames.hasNext()) {
+        String updatedFieldName = fieldNames.next();
+        JsonNode valueToBeUpdated = mainNode.get(updatedFieldName);
+        JsonNode updatedValue = updateNode.get(updatedFieldName);
+
+        // If the node is an @ArrayNode
+        if (valueToBeUpdated != null && valueToBeUpdated.isArray() && 
+            updatedValue.isArray()) {
+            // running a loop for all elements of the updated ArrayNode
+            for (int i = 0; i < updatedValue.size(); i++) {
+                JsonNode updatedChildNode = updatedValue.get(i);
+                // Create a new Node in the node that should be updated, if there was no corresponding node in it
+                // Use-case - where the updateNode will have a new element in its Array
+                if (valueToBeUpdated.size() <= i) {
+                    ((ArrayNode) valueToBeUpdated).add(updatedChildNode);
+                }
+                // getting reference for the node to be updated
+                JsonNode childNodeToBeUpdated = valueToBeUpdated.get(i);
+                merge(childNodeToBeUpdated, updatedChildNode);
+            }
+        // if the Node is an @ObjectNode
+        } else if (valueToBeUpdated != null && valueToBeUpdated.isObject()) {
+            merge(valueToBeUpdated, updatedValue);
+        } else {
+            if (mainNode instanceof ObjectNode) {
+                ((ObjectNode) mainNode).replace(updatedFieldName, updatedValue);
+            }
         }
     }
-    
-    @RequestMapping(value = "/rest/stencil-sets/cmmneditor", method = RequestMethod.GET, produces = "application/json")
-    public JsonNode getCmmnStencilSetForEditor() {
-        try {
-            JsonNode stencilNode = objectMapper.readTree(this.getClass().getClassLoader().getResourceAsStream("stencilset_cmmn.json"));
-            return stencilNode;
-        } catch (Exception e) {
-            LOGGER.error("Error reading bpmn stencil set json", e);
-            throw new InternalServerErrorException("Error reading bpmn stencil set json");
-        }
-    }
+    return mainNode;
+}
 }
