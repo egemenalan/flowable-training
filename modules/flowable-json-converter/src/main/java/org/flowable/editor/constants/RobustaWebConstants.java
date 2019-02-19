bc803a43949e4ba1f331792bbd104d3ff29a81b9
egemen.alan <egemen.alan@robusta.com.tr>
2019-02-19 15:41:44 +0300

    robusta web constant

---
+++ modules/flowable-json-converter/src/main/java/org/flowable/editor/constants/RobustaWebConstants.java
@@ -0,0 +1,22 @@
+package org.flowable.editor.constants;
+
+/*
+ * Author Egemen ALAN
+ */
+
+public interface RobustaWebConstants {
+
+	String STENCIL_TASK_WEBAPP = "CustomWebAppTask";
+	String STENCIL_TASK_WEBMOUSE = "CustomWebMouseTask";
+	String STENCIL_TASK_WEBWAIT = "CustomWebWaitTask";
+	String STENCIL_TASK_WEBSET = "CustomWebSetTask";
+	String STENCIL_TASK_WEBGET = "CustomWebGetTask";
+	String STENCIL_TASK_WEBCLOSE = "CustomWebCloseTask";
+	String STENCIL_TASK_WEBSCROLL = "CustomWebScrollTask";
+	String STENCIL_TASK_WEBALERT = "CustomWebAlertTask";
+	String STENCIL_TASK_WEBSWITCH = "CustomWebSwitchTask";
+	String STENCIL_TASK_WEBDOWNLOAD = "CustomWebDownloadTask";
+	String STENCIL_TASK_WEBCAPTURE = "CustomWebCaptureTask";
+	String STENCIL_TASK_WEBFUNCTION = "CustomWebFunctionTask";
+
+}
